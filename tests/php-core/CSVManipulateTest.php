757cc15c284a4b2cc11f7c2f022faad40f8a9ec5
Sean Chen <sean.chen@leocorn.com>
2017-11-10 13:14:00 -0500

    add more testing and assert

--- tests/php-core/CSVManipulateTest.php
+++ tests/php-core/CSVManipulateTest.php
@@ -11,19 +11,24 @@ class StrGetcsvTest extends TestCase {
 
     public function testBasicParseCSV() {
 
-        $csvString = "abc,1,,3,again";
+        $csvString = "abc,1,,3,again,";
 
         $result = str_getcsv($csvString);
 
         // the result should be an array.
         $this->assertTrue(gettype($result) === "array");
 
-        // verify.
+        // check the size of the array.
+        $this->assertEquals(count($result), 6);
+
+        // verify value of the first column
         $this->assertEquals($result[0], 'abc');
         // each column will be string type.
         $this->assertEquals($result[1], '1');
         // empty column will be null
         $this->assertEquals($result[2], null);
+        // we could use empty function to check.
         $this->assertTrue(empty($result[2]));
+        $this->assertTrue(empty($result[5]));
     }
 }
