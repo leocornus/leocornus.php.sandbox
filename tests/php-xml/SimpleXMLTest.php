fefe24dcd45f0d04a785f45aabe2694ca4ce7681
Sean Chen <sean.chen@leocorn.com>
2017-11-11 20:50:50 -0500

    SimpleXMLElement is case censitive.

--- tests/php-xml/SimpleXMLTest.php
+++ tests/php-xml/SimpleXMLTest.php
@@ -11,16 +11,46 @@ class SimpleXMLTest extends TestCase {
     public function testLoadXMLFromString() {
 
         $xmlString = <<<EOT
-<photos><boardid>2</boardid><listingnumber>40795639</listingnumber><count>9</count><photo><mainlink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/0/0/0/5778214fa97b2267dc0984532d829256/46/bc980b69550331cf400d63cc8ecd1969/40795639.JPG</mainlink><thumblink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/0/0/0/5778214fa97b2267dc0984532d829256/46/bc980b69550331cf400d63cc8ecd1969/40795639.JPG</thumblink><largelink></largelink><priority>0</priority></photo><photo><mainlink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/1/0/0/e0866092fe22ee3e527cfdae6e5e0cbc/46/bc980b69550331cf400d63cc8ecd1969/40795639-1.JPG</mainlink><thumblink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/1/0/0/e0866092fe22ee3e527cfdae6e5e0cbc/46/bc980b69550331cf400d63cc8ecd1969/40795639-1.JPG</thumblink><largelink></largelink><priority>1</priority></photo><photo><mainlink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/2/0/0/356327509475f89470646af8fda11bed/46/bc980b69550331cf400d63cc8ecd1969/40795639-2.JPG</mainlink><thumblink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/2/0/0/356327509475f89470646af8fda11bed/46/bc980b69550331cf400d63cc8ecd1969/40795639-2.JPG</thumblink><largelink></largelink><priority>2</priority></photo><photo><mainlink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/3/0/0/3af660350c51d29d5c84ed683d9028f5/46/bc980b69550331cf400d63cc8ecd1969/40795639-3.JPG</mainlink><thumblink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/3/0/0/3af660350c51d29d5c84ed683d9028f5/46/bc980b69550331cf400d63cc8ecd1969/40795639-3.JPG</thumblink><largelink></largelink><priority>3</priority></photo><photo><mainlink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/4/0/0/ed9bfae261022a9e3b859c934b4a877a/46/bc980b69550331cf400d63cc8ecd1969/40795639-4.JPG</mainlink><thumblink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/4/0/0/ed9bfae261022a9e3b859c934b4a877a/46/bc980b69550331cf400d63cc8ecd1969/40795639-4.JPG</thumblink><largelink></largelink><priority>4</priority></photo><photo><mainlink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/5/0/0/bc6dba9a7274187ca2b1b768a7a27b86/46/bc980b69550331cf400d63cc8ecd1969/40795639-5.JPG</mainlink><thumblink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/5/0/0/bc6dba9a7274187ca2b1b768a7a27b86/46/bc980b69550331cf400d63cc8ecd1969/40795639-5.JPG</thumblink><largelink></largelink><priority>5</priority></photo><photo><mainlink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/6/0/0/1823debed221d480d9dda4ba1b8deb7d/46/bc980b69550331cf400d63cc8ecd1969/40795639-6.JPG</mainlink><thumblink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/6/0/0/1823debed221d480d9dda4ba1b8deb7d/46/bc980b69550331cf400d63cc8ecd1969/40795639-6.JPG</thumblink><largelink></largelink><priority>6</priority></photo><photo><mainlink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/7/0/0/700b2070c7f523068fdaa63936d4cdda/46/bc980b69550331cf400d63cc8ecd1969/40795639-7.JPG</mainlink><thumblink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/7/0/0/700b2070c7f523068fdaa63936d4cdda/46/bc980b69550331cf400d63cc8ecd1969/40795639-7.JPG</thumblink><largelink></largelink><priority>7</priority></photo><photo><mainlink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/8/0/0/66d4b66c09cf2104728919c127545504/46/bc980b69550331cf400d63cc8ecd1969/40795639-8.JPG</mainlink><thumblink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/8/0/0/66d4b66c09cf2104728919c127545504/46/bc980b69550331cf400d63cc8ecd1969/40795639-8.JPG</thumblink><largelink></largelink><priority>8</priority></photo></photos>
+<?xml version="1.0" encoding="utf-8"?>
+<Photos>
+  <BoardID>2</BoardID>
+  <ListingNumber>40783548</ListingNumber>
+  <Count>4</Count>
+  <Photo>
+    <MainLink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40783548/0/0/0/3427d74baa0336da86b845344b16dcc1/63/a50384b1d17814b288aa7a851a6b0d5d/40783548.JPG</MainLink>
+    <ThumbLink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40783548/0/0/0/3427d74baa0336da86b845344b16dcc1/63/a50384b1d17814b288aa7a851a6b0d5d/40783548.JPG</ThumbLink>
+    <LargeLink/>
+    <Priority>0</Priority>
+  </Photo>
+  <Photo>
+    <MainLink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40783548/1/0/0/565e6c0510daf1e5e09627b893362f01/63/a50384b1d17814b288aa7a851a6b0d5d/40783548-1.JPG</MainLink>
+    <ThumbLink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40783548/1/0/0/565e6c0510daf1e5e09627b893362f01/63/a50384b1d17814b288aa7a851a6b0d5d/40783548-1.JPG</ThumbLink>
+    <LargeLink/>
+    <Priority>1</Priority>
+  </Photo>
+  <Photo>
+    <MainLink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40783548/2/0/0/f14840e102e694ce592b489dcfa04a96/63/a50384b1d17814b288aa7a851a6b0d5d/40783548-2.JPG</MainLink>
+    <ThumbLink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40783548/2/0/0/f14840e102e694ce592b489dcfa04a96/63/a50384b1d17814b288aa7a851a6b0d5d/40783548-2.JPG</ThumbLink>
+    <LargeLink/>
+    <Priority>2</Priority>
+  </Photo>
+  <Photo>
+    <MainLink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40783548/3/0/0/2149b9e042554ce9f1fa84f2c1902f0b/63/a50384b1d17814b288aa7a851a6b0d5d/40783548-3.JPG</MainLink>
+    <ThumbLink>http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40783548/3/0/0/2149b9e042554ce9f1fa84f2c1902f0b/63/a50384b1d17814b288aa7a851a6b0d5d/40783548-3.JPG</ThumbLink>
+    <LargeLink/>
+    <Priority>3</Priority>
+  </Photo>
+</Photos>
 EOT;
 
         // string to xml.
         $theXML = simplexml_load_string($xmlString);
+        //var_dump($theXML);
 
         // check element text.
-        $this->assertEquals($theXML->boardid, "2");
-        var_dump($theXML->photo);
-        $this->assertEquals($theXML->photo->mainlink, "http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/0/0/0/5778214fa97b2267dc0984532d829256/46/bc980b69550331cf400d63cc8ecd1969/40795639.JPG");
-        $this->assertEquals($theXML->photo->thumblink, "http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40795639/0/0/0/5778214fa97b2267dc0984532d829256/46/bc980b69550331cf400d63cc8ecd1969/40795639.JPG");
+        $this->assertEquals($theXML->BoardID, "2");
+        //var_dump($theXML->Photo);
+        $this->assertEquals($theXML->Photo[0]->MainLink, "http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40783548/0/0/0/3427d74baa0336da86b845344b16dcc1/63/a50384b1d17814b288aa7a851a6b0d5d/40783548.JPG");
+        $this->assertEquals($theXML->Photo[0]->ThumbLink, "http://cdnparap30.paragonrels.com/ParagonImages/Property/P3/MAXEBRDI/40783548/0/0/0/3427d74baa0336da86b845344b16dcc1/63/a50384b1d17814b288aa7a851a6b0d5d/40783548.JPG");
     }
 }
