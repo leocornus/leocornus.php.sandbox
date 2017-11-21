1bb09e33fc0f02dab5abebab04858722a2f83097
Sean Chen <sean.chen@leocorn.com>
2017-11-21 10:32:24 -0500

    testing the str_pad to format numbers

--- tests/php-core/NumberTest.php
+++ tests/php-core/NumberTest.php
@@ -11,6 +11,10 @@ use PHPUnit\Framework\TestCase;
 
 class NumberTest extends TestCase {
 
+    /**
+     * number_format will handle float number decimals and
+     * some current format.
+     */
     public function testNumberFormat() {
 
         $strNumber = "325000.0000000";
@@ -26,5 +30,27 @@ class NumberTest extends TestCase {
         // format number to integer.
         $formatNumber = number_format($floatNumber, 0);
         $this->assertEquals($formatNumber, "325,000");
+    }
+
+    /**
+     * using str_pad to adding 0 in front of an integer.
+     *
+     */
+    public function testStringPad() {
+
+        // the input number.
+        $numbers = [1, 12, 123];
+
+        // the formated result.
+        $formatted = [];
+
+        foreach($numbers as $number) {
+            // adding leading 0 
+            $formatted[] = str_pad($number, 3, "0", STR_PAD_LEFT);
+        }
+
+        $this->assertEquals($formatted[0], '001');
+        $this->assertEquals($formatted[1], '012');
+        $this->assertEquals($formatted[2], '123');
     }
 }
