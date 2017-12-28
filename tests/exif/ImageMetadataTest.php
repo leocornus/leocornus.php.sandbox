2af68dbd2d6ada72276e9549de8a2c47307792ca
Sean Chen <sean.chen@leocorn.com>
2017-12-28 10:32:24 -0500

    try to get a list of basic Exif metadata

--- tests/exif/ImageMetadataTest.php
+++ tests/exif/ImageMetadataTest.php
@@ -13,24 +13,45 @@ use PHPUnit\Framework\TestCase;
 
 class ImageMetadataTest extends TestCase {
 
-    /** 
-     * try to get image type.
-     */
-    public function testImagetype() {
+    // define the image file path.
+    protected static $imageFile;
+
+    public static function setUpBeforeClass() {
 
         // assume the testing images are in the same folder.
         $folder = dirname(__FILE__);
         //echo $folder;
         // file name is case sentive!
-        $imageFile = $folder . "/20140127.JPG";
+        self::$imageFile = $folder . "/20140127.JPG";
+    }
+
+    /** 
+     * try to get image type.
+     */
+    public function testImagetype() {
 
         // get the imageType
-        $imageType = exif_imagetype($imageFile);
-        
+        $imageType = exif_imagetype(self::$imageFile);
+
         // 2 will be jpeg type.
         $this->assertEquals($imageType, 2);
 
         // full list of image types could be found on page:
         // http://php.net/manual/en/function.exif-imagetype.php
+    }
+
+    /**
+     * get a list of metadata.
+     */
+    public function testGetMetadata() {
+
+        // read all metadata using default settings.
+        //echo self::$imageFile;
+        // Add the @ operator to ignore some errors.
+        $exif = @exif_read_data(self::$imageFile);
+        print_r($exif);
+
+        // check the file name.
+        $this->assertEquals($exif['FileName'], '20140127.JPG');
     }
 }
