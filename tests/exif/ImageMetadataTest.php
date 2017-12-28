e106b1c671f9ddb90f693f12c102cf6c2526a3aa
Sean Chen <sean.chen@leocorn.com>
2017-12-28 09:20:00 -0500

    get started to learn the EXIF functions

---
+++ tests/exif/ImageMetadataTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * quick test to explore the metadata from Exchangeable Image File (EXIF) format.
+ *
+ * Exif version 2.2 http://exif.org/Exif2-2.PDF,
+ * It was established in April, 2002
+ *
+ * PHP language need compiled with --enable-exif and --enable-mbstring to 
+ * suport all exif functions.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class ImageMetadataTest extends TestCase {
+
+}
