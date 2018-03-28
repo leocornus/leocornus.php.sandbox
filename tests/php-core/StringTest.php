ec4124b2fbc2a5db72891168e4962b828f11cc5b
Sean Chen <sean.chen@leocorn.com>
2018-03-28 15:51:41 +0000

    test cases for basic functions to manipulate strings.

---
+++ tests/php-core/StringTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * learn php functions to manipulate strings.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class StringTest extends TestCase {
+
+    public function testCapitalize() {
+
+        $src = "hello world";
+
+        // capitalize
+        $ret = ucfirst($src);
+        $this->assertEquals($ret, "Hello world");
+
+        // capitalize each word.
+        $ret = ucwords($src);
+        $this->assertEquals($ret, "Hello World");
+    }
+}
